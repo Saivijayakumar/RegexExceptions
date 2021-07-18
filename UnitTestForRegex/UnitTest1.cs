775e1d5e1b9cfb5d016ef99a33f58b74c3cbdfa9
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-18 22:07:38 +0530

    [SaiVijayaKumar] Validating First Name

---
+++ UnitTestForRegex/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UsingExceptionsInRegex;
+
+namespace UnitTestForRegex
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Test for valid first name
+        [TestMethod]
+        public void TestMethodforValidFirstName()
+        {
+            string expected = "Valid";
+            string actual = RegexAllMethods.CheckFirstName("Ram");
+            Assert.AreEqual(expected, actual);
+
+        }
+        // Test for invalid first name
+        [TestMethod]
+        public void TestMethodforInValidFirstName()
+        {
+            string expected = "Invalid";
+            string actual = RegexAllMethods.CheckFirstName("Ra");
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ UsingExceptionsInRegex/RegexAllMethods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UsingExceptionsInRegex
+{
+    public class RegexAllMethods
+    {
+        string message;
+        //Default Constructor
+        public RegexAllMethods()
+        {
+
+        }
+        //Parameterized Constructor
+        public RegexAllMethods(string message)
+        {
+            this.message = message;
+        }
+        //checking first name valid or not 
+        public static string CheckFirstName(string firstName)
+        {
+            string result = "Valid";
+            Regex regex = new Regex(@"^[A-Z]{1}[a-z]{2,}");
+            Match match = regex.Match(firstName);
+            if (match.Success)
+            {
+                Console.WriteLine(firstName + " ------>Valid");
+            }
+            else
+            {
+                Console.WriteLine(firstName + " ------>Invalid");
+                result = "Invalid";
+            }
+            return result;
+        }
+    }
+}
